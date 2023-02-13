433ad70282a77a668fbc314430571234f25028f0
Patrick <patrick@Patricks-MacBook-Pro.local>
2023-02-13 01:21:09 -0300

    build options

--- src/main/java/nascimento/patrick/main/ConsoleApplication.java
+++ src/main/java/nascimento/patrick/main/ConsoleApplication.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 import nascimento.patrick.restaurantsfinder.RestaurantsFinder;
 import nascimento.patrick.restaurantsfinder.csv.CSVLoader;
 import nascimento.patrick.restaurantsfinder.model.Cuisine;
@@ -15,18 +17,45 @@ public class ConsoleApplication {
 
 		RestaurantsFinder finder = new RestaurantsFinder(CSVLoader.getRestaurants());
 
-		boolean consoleApplication = true;
+		boolean consoleApplication = args.length == 0;
 
 		if (consoleApplication) {
 			startConsoleApplication(finder);
+		} else {
+			startSingleSearch(finder, args);
 		}
+	}
+
+	private static void startSingleSearch(RestaurantsFinder finder, String[] args) {
+		if (args.length == 5) {
+			String name = StringUtils.isEmpty(args[0]) ? null : args[0];
+			String cuisine = StringUtils.isEmpty(args[1]) ? null : args[1];
+			Integer distance = StringUtils.isEmpty(args[2]) ? null : Integer.parseInt(args[2]);
+			Integer customerRating = StringUtils.isEmpty(args[3]) ? null : Integer.parseInt(args[3]);
+			Integer price = StringUtils.isEmpty(args[4]) ? null : Integer.parseInt(args[4]);
+
+			Restaurant target = new Restaurant();
+			target.setName(name);
+			target.setCuisine(cuisine != null ? new Cuisine(0, cuisine) : null);
+			target.setDistance(distance);
+			target.setCustomerRating(customerRating);
+			target.setPrice(price);
+
+			List<Restaurant> restaurants = finder.find(target);
+
+			printResults(restaurants);
+
+		} else {
+			System.out.println("The system expects 5 arguments, each separated with comas.");
+
+		}
+
 	}
 
 	private static void startConsoleApplication(RestaurantsFinder finder) {
 		int option = 0;
 		try (Scanner scanner = new Scanner(System.in)) {
 			while (option != 4) {
-
 
 				System.out.println("Welcome to Restaurants Finder! ");
 				System.out.println("");
@@ -37,8 +66,9 @@ public class ConsoleApplication {
 				System.out.println("  4 - exit application		");
 				System.out.println("");
 
-				option = scanner.nextInt(); scanner.nextLine();
-				
+				option = scanner.nextInt();
+				scanner.nextLine();
+
 				switch (option) {
 				case 1:
 					findRestaurantMenu(scanner, finder);
@@ -66,8 +96,6 @@ public class ConsoleApplication {
 		}
 
 	}
-
-
 
 	private static void findRestaurantMenu(Scanner scanner, RestaurantsFinder finder) {
 		System.out.println("");
@@ -97,6 +125,11 @@ public class ConsoleApplication {
 
 		List<Restaurant> restaurants = finder.find(target);
 
+		printResults(restaurants);
+
+	}
+
+	private static void printResults(List<Restaurant> restaurants) {
 		switch (restaurants.size()) {
 		case 0:
 			System.out.println("Sorry, we didn't find any restaurant with this filter, please try again. ");
@@ -113,10 +146,7 @@ public class ConsoleApplication {
 		}
 
 		System.out.println("");
-
 	}
-
-
 
 	private static Integer readInteger(Scanner scanner) {
 		String line = scanner.nextLine();
@@ -133,10 +163,11 @@ public class ConsoleApplication {
 		}
 		return line;
 	}
-	
+
 	private static void printRestaurants(List<Restaurant> restaurants) {
 		restaurants.forEach(restaurant -> System.out.println(restaurant));
 	}
+
 	private static void printCuisines(Map<String, Cuisine> cuisines) {
 		cuisines.values().forEach(cuisine -> System.out.println(cuisine));
 	}
